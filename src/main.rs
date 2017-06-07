b1d4c00454373edba9f33b579e4a9277c41f22e0
junhwong <jun@diosay.com>
2017-06-08 00:52:19 +0800

    add child thread for test stacks

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,25 @@
 extern crate tera;
 
 fn main() {
+    use std::env;
+    use std::thread;
     use tera::Tera;
-    let mut ctx = tera::Context::new();
-    //panic!("never returns!");
-    let tera = compile_templates!("templates/*");
-    tera.render("index.html", &ctx);
+    let child = thread::Builder::new().stack_size(2 * 1024 * 1024).spawn(move || {
+        let mut ctx = tera::Context::new();
+        let tera = compile_templates!("templates/*");
+        tera.render("index.html", &ctx);
+        println!("PASSED");
+    }).unwrap();
+    println!("min_stack:{:?}", env::var("RUST_MIN_STACK"));
+    println!("max_cached_stacks:{:?}", env::var("RUST_CACHED_STACKS"));
+    child.join().unwrap();
 }
 
 #[test]
 fn test() {
+    
     use tera::Tera;
+    
     let mut ctx = tera::Context::new();
     //panic!("never returns!");
     let tera = compile_templates!("templates/*");
